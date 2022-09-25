d3841774b218c1c88ff7c6a4ee8c2d4bf4aad6a7
Kalixer <al.caceres.ag72@gmail.com>
2022-09-25 17:41:37 -0300

    Next Image

--- next.config.js
+++ next.config.js
@@ -1,19 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    customKey: 'customValue',
-  },
-  basePath: '/dist',
-  compress: true,
-  async redirects() {
-    return [
-      {
-        source: '/hola',
-        destination: 'https://www.youtube.com',
-        permanent: true
-      }
-    ]
-  }
 }
 
 
