bfa76be1639f3f3568172d31d86e003d1989e2fd
Kalixer <al.caceres.ag72@gmail.com>
2022-11-21 19:22:52 -0300

    Minor changes according to the 'Correccion de bugs' class

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -19,7 +19,7 @@ const MyOrder = () => {
 		<aside className={styles.MyOrder}>
 			<div className={styles['MyOrder-container']}>
 				<div className={styles['title-container']}>
-					<Image className={styles['more-clickable-area'], styles.pointer} src={arrow} alt="arrow" onClick={() => toggleOrder()} />
+					<Image className={(styles['more-clickable-area'], styles.pointer)} src={arrow} alt="arrow" onClick={() => toggleOrder()} />
 					<p className={styles.title}>My order</p>
 				</div>
 				<div className={styles['my-order-content']}>
